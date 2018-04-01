abdfdf9c6a98ee2479ad2e0574837eabbf667170
Saad <saadbennmushtaq@gmail.com>
2018-03-31 20:04:52 -0500

    Fix how object methods are referenced.

--- Trie.py
+++ Trie.py
@@ -1,9 +1,9 @@
-import node
+from node import Node
 
 
 class Trie:
     def __init__(self):
-        self.root = node('*')
+        self.root = Node('*')
 
     def add(self, word):
         """
@@ -13,13 +13,14 @@ class Trie:
 
         for letter in word:
             found_in_child = False
+            children = root_node.get_children
 
-            for child in root_node.get_children():
+            for child in children:
                 # compare letters
-                if child.get_char() == letter:
+                if child.get_char == letter:
                     # We found it, increase the counter by 1 to keep track that another
                     # word has it as well
-                    root_node.increment_count()
+                    child.increment_count
                     # And point the node to the child that contains this char
                     root_node = child
                     found_in_child = True
@@ -27,13 +28,15 @@ class Trie:
 
             # We did not find it so add a new child
             if not found_in_child:
-                new_node = node(letter)
-                root_node.get_children().append(new_node)
+                new_node = Node(letter)
+
+                children.append(new_node)
                 # And then point node to the new child
                 root_node = new_node
 
         # Everything finished. Mark it as the end of a word.
-        node.word_finished = True
+        value = True
+        root_node.is_word_finished = value
 
     def find_prefix(self, prefix):
         """
@@ -43,15 +46,15 @@ class Trie:
         """
         node = self.root
 
-        if not node.get_children():
+        if not node.get_children:
             return False, 0
 
         for letter in prefix:
             char_not_found = True
 
             # Search through all the children of the present `node`
-            for child in node.get_children():
-                if child.get_char() == letter:
+            for child in node.get_children:
+                if child.get_char == letter:
                     # We found the char existing in the child.
                     char_not_found = False
                     # Assign node as the child containing the char and break
@@ -64,4 +67,4 @@ class Trie:
         # Well, we are here means we have found the prefix. Return true to indicate that
         # And also the counter of the last node. This indicates how many words have this
         # prefix
-        return True, node.get_count()
+        return True, node.get_count
